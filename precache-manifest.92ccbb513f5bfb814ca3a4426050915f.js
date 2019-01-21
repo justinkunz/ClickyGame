554c44c171e72ceede683dac3459fc636c48bc2d
Justin Kunz <Justin_Kunz@yahoo.com>
2019-01-20 23:55:35 -0600

    Updates

---
+++ precache-manifest.92ccbb513f5bfb814ca3a4426050915f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClickyGame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "af354cf2841fae37e009",
+    "url": "/ClickyGame/static/js/main.af354cf2.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "/ClickyGame/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "af354cf2841fae37e009",
+    "url": "/ClickyGame/static/css/main.f034dcd4.chunk.css"
+  },
+  {
+    "revision": "8473add0aefc6788d2a22df3f723a688",
+    "url": "/ClickyGame/index.html"
+  }
+];
